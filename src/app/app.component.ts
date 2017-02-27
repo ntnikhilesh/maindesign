c910f2cfd42e10d7fb465111ef6216a08dd02971
nikhil <ntnikhilesh@gmail.com>
2017-02-27 16:05:01 +0530

    Tooltip added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hello Ravi';
+  title = 'Hello from CC...';
+  position = 'above';
 }
