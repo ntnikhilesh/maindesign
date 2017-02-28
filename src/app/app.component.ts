e61dfc6b79300a6c5b73ac2a0efb28e4557ab9d2
nikhil <ntnikhilesh@gmail.com>
2017-02-28 14:25:40 +0530

    bill added using ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.dcomponent.html',
-  styleUrls: ['./app.dcomponent.css']
+  templateUrl: './app.ngfordemo.html',
+  styleUrls: ['./app.ngfordemo.css']
 })
 export class AppComponent {
   title = 'Hello Ravi';
-}
+
+  /*ngForDemo */
+  heroes = ['Bill-12', 'Bill-32']
+  } 
+
+
