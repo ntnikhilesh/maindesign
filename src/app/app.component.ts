074cd70aa931151df17de0023d05037b1d9e21fb
nikhil <ntnikhilesh@gmail.com>
2017-02-28 15:23:20 +0530

    Add bill added as demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.ngfordemo.html',
-  styleUrls: ['./app.ngfordemo.css']
+  templateUrl: './app.eventbinding.html',
+  styleUrls: ['./app.eventbinding.css']
 })
 export class AppComponent {
   title = 'Hello Ravi';
 
   /*ngForDemo */
-  heroes = ['Bill-12', 'Bill-32']
-  } 
+  /*heroes = ['Bill-12', 'Bill-32'] */
+
+  /* Event Binding */
+ 
+heroes = ['Bill-12'];
+  addHero(newHero: string) {
+    if (newHero) {
+      this.heroes.push(newHero);
+    }
+  }
+
+  }
+  
 
 
